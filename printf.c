d058fb3854d0ea436e5c76c4b22ce06c2bce66b6
Daldanos <adolfogsilveira@gmail.com>
2023-11-14 06:58:42 -0800

    Printf

---
+++ printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+
+
+int _printf(const char *format, ...)
+{
+	int i, scont, cont = 0;
+	va_list args;
+	va_start(args, format);
+
+	for (i = 0; format[i] != '\0'; i++) // initalize variable i for counter, and iterate the array
+	{
+		if (format[i] != NULL) // verify if the array had content
+		{
+			if (format[i] == '%') // checks for %
+			{
+				i++;
+			}
+			if (format[i] == 'c') // checks type of content
+			{
+				while (format[i + 1] != NULL) // iterate the string
+				{
+					i++;
+					_putchar(va_args(args, char); // prints the position of the string
+				}
+			}
+			else if (format[i] == 's')
+			{
+				scont = putss(va_args(args, *char));
+				i++;
+				cont += (scont -1);
+			}
+			else if (format[i] == '%')
+			{
+				_putchar("\%");
+			
+			}
+		}
+	}
+	va_end(args);
+	return (cont);
+}
