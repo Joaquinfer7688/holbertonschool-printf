2b798e1217bc6c34c8a0805665c5dc428a1b8c80
Daldanos <adolfogsilveira@gmail.com>
2023-11-17 11:25:23 -0800

    Printf

--- aux.c
+++ aux.c
@@ -48,15 +48,13 @@ long print_number(long num)
 	int count = 0;
 	char digit;
 
-	if (num < 0)
+	if (num / 10 != 0)
+		count += print_number(num / 10);
+	else if (num < 0)
 	{
 		write(1, "", 1);
 		count++;
-		num = -num;
 	}
-
-	if (num / 10 != 0)
-		count += print_number(num / 10);
 
 	digit = (num % 10) + '0';
 	write(1, &digit, 1);
