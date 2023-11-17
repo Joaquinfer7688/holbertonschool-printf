68a165cde877812ac1c7c95170513bc1ee3d6806
Daldanos <adolfogsilveira@gmail.com>
2023-11-17 11:07:06 -0800

    Printf

--- aux.c
+++ aux.c
@@ -43,7 +43,7 @@ int process_int(va_list args)
  * @num: Non-negative integer
  * Return: Number of digits printed
  */
-int print_number(int num)
+long print_number(long num)
 {
 	int count = 0;
 	char digit;
