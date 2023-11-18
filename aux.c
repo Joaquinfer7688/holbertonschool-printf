16013e652a7d581c9c5d5f4198a0a0314206ab7a
Joaquinfer7688 <7688@holbertonschool.com>
2023-11-18 10:48:16 -0800

    printf

--- aux.c
+++ aux.c
@@ -23,10 +23,10 @@ int process_str(const char *str)
  */
 long int process_int(va_list args)
 {
-        int num = va_arg(args, int);
-        int count = 0;
+	long int num = va_arg(args, long int);
+	int count = 0;
 
-        if (num < 0)
+	if (num < 0)
         {
                 write(1, "-", 1);
                 num = -num;
